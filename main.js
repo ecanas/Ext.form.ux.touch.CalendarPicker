44b13c2edd3d6fd8da6fed9acbec09781ef5618c
Edgar Canas <ecanas@ordersinseconds.com>
2011-02-03 14:40:08 -0500

    Code Cleanup

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ Ext.setup({
 						{
 							xtype: "calendarpickerfield",
 							label: "Choose Date",
-							dateFormat: 'MM/dd/yy',
+							dateFormat: 'm-d-Y',
 							value: new Date()
 						}
 					]
